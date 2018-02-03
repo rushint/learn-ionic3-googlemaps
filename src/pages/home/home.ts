7f2c7530136e2d8714c420b1a660656cb8edd999
Theo Rushin <rushint@yahoo.com>
2018-02-02 22:11:51 -0500

    My First Commit

---
+++ src/pages/home/home.ts
@@ -0,0 +1,66 @@
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { IonicPage } from 'ionic-angular';
+import { NavController } from 'ionic-angular';
+
+import { Geolocation } from '@ionic-native/geolocation';
+
+declare var google;
+
+@IonicPage()
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+    @ViewChild('map') mapElement: ElementRef;
+  
+    map: any;
+
+    constructor(public navCtrl: NavController, public geolocation: Geolocation) {
+    }
+
+    ionViewDidLoad(){
+        this.loadMap();
+    }
+
+    loadMap() { 
+        this.geolocation.getCurrentPosition().then((position) => {
+            let latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+
+            let mapOptions = {
+                center: latLng,
+                zoom: 15,
+                mapTypeId: google.maps.MapTypeId.ROADMAP
+            }
+        
+            this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+
+            this.addMarker();
+        }, (err) => {
+            console.log(err);
+        });
+    }
+
+    addMarker(){
+        let marker = new google.maps.Marker({
+            map: this.map,
+            animation: google.maps.Animation.DROP,
+            position: this.map.getCenter()
+        });
+        
+        let content = '<p>"No matter where you go, there you are."</p>';         
+        
+        this.addInfoWindow(marker, content);
+    }
+
+    addInfoWindow(marker, content){
+        let infoWindow = new google.maps.InfoWindow({
+            content: content
+        });
+        
+        google.maps.event.addListener(marker, 'click', () => {
+            infoWindow.open(this.map, marker);
+        });
+    }
+}
